b4811983046cc9dfb37c65b9306c810f8196c42c
ozcer <iamreallyoscar@hotmail.com>
2018-04-15 11:47:22 -0700

    start code

---
+++ main.py
@@ -0,0 +1,72 @@
+import logging
+import os
+import random
+import sys
+
+import pygame
+from pygame.locals import *
+from src.const import *
+
+
+
+class Game:
+    
+    def __init__(self):
+        # pygame window setups
+        os.environ['SDL_VIDEO_CENTERED'] = '1'  # center window
+        pygame.init()
+        pygame.display.set_caption(CAPTION)
+        self.surface = pygame.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT), 0, 32)
+        logging.basicConfig(level=LOG_LEVEL,
+                            datefmt='%m/%d/%Y %I:%M:%S%p',
+                            format='%(asctime)s %(message)s')
+
+        self.entities = {ALL_SPRITES: pygame.sprite.Group()}
+        self.fps_clock = pygame.time.Clock()
+        self.events = pygame.event.get()
+        
+        self.run()
+    
+    def run(self):
+        while True:
+            self.surface.fill(L_GREY)
+            
+            self.update_all_sprites()
+            self.draw_all_sprites()
+
+            # fps and update display
+            pygame.display.update()
+            self.fps_clock.tick(FPS)
+            
+            self.events = pygame.event.get()
+            for event in self.events:
+                if event.type == QUIT:
+                    pygame.quit()
+                    sys.exit()
+    
+    def update_all_sprites(self):
+        # update all objects
+        for sprite in self.entities[ALL_SPRITES]:
+            sprite.update()
+    
+    def draw_all_sprites(self):
+        # draw abased on depth
+        for sprite in sorted(self.entities[ALL_SPRITES],
+                             key=lambda sprite: sprite.depth,
+                             reverse=True):
+            sprite.draw()
+    
+    def add_entity(self, object):
+        # add to its own sprite group
+        class_name = object.__class__.__name__
+        if class_name not in self.entities:
+            self.entities[class_name] = pygame.sprite.Group()
+        self.entities[class_name].add(object)
+        logging.info(f"{object} created")
+        
+        # also add to global sprite group
+        self.entities[ALL_SPRITES].add(object)
+    
+
+if __name__ == "__main__":
+    Game()
---
+++ src/const.py
@@ -0,0 +1,30 @@
+# display
+FPS = 60
+CAPTION = "Project Ooze"
+DISPLAY_WIDTH = 1280
+DISPLAY_HEIGHT = 540
+START_WINDOW_POS = (100,100)
+
+# logging
+LOG_LEVEL = "NOTSET"
+
+# colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+D_GREY = (40, 40, 40)
+L_GREY = (100, 100, 100)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+YELLOW = (255, 255, 0)
+ORANGE = (244,179, 66)
+
+OLIVE = (130, 119, 23)
+D_OLIVE = (82, 76, 0)
+L_OLIVE = (180, 166, 71)
+
+BLUE = (25, 118, 210)
+L_BLUE = (99, 164, 255)
+D_BLUE = (0, 75, 160)
+
+# group of all sprite
+ALL_SPRITES = "ALL"
