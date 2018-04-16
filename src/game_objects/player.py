e067ec46c05a8c3f6cdd684c0f37a3436090b9c1
ozcer <iamreallyoscar@hotmail.com>
2018-04-15 19:45:43 -0700

    small camera fix

--- src/game_objects/player.py
+++ src/game_objects/player.py
@@ -30,9 +30,9 @@ class Player(Dynamic):
 
     def process_event(self):
         keys = pygame.key.get_pressed()  # checking pressed keys
-        if keys[pygame.K_RIGHT]:
+        if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
             self.move("right")
-        if keys[pygame.K_LEFT]:
+        elif keys[pygame.K_LEFT] or keys[pygame.K_a]:
             self.move("left")
 
     def move(self, direction):
