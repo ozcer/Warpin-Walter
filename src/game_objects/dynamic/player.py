8b352a678c44e2518d84b5e520538e86b129127b
ozcer <iamreallyoscar@hotmail.com>
2018-04-20 19:53:04 -0700

    on contact with goal builds next level

--- src/game_objects/dynamic/player.py
+++ src/game_objects/dynamic/player.py
@@ -37,6 +37,8 @@ class Player(Dynamic):
         self.set_image("idle")
         self.ticks_per_frame = 5
         
+        self.won = False
+        
 
     def update(self):
         super().update()
@@ -55,6 +57,9 @@ class Player(Dynamic):
 
         if self.contact_with(Enemy, "left") or self.contact_with(Enemy, "right"):
             self.get_hit(10)
+        
+        if self.won:
+            self.game.build_next_level()
         
         if self.on_ground():
             if self.dx == 0:
